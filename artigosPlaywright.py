a3551c8aa374b305ef5a86fb4654f696d600a4c3
Yan Sardinha <yansardinha1001@gmail.com>
2022-09-20 20:55:39 -0300

    Corrigindo variáveis e adicionando ideias a se seguir

--- artigosPlaywright.py
+++ artigosPlaywright.py
@@ -1,35 +1,47 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 
+def getInfo(html):
+    title_list = []
+    authour_list = []
+    background_list = []
+    tag_list = []
+    complete_version_list = []
+    pass
+
 def getArticles(html):
-        browser = p.firefox.launch(headless=False)
-        page = browser.new_page()
-        page.goto(html)
-        page.is_visible('div.tile-body')
-        current_page = page.url
-        next_page = page.locator("text=>").nth(1).get_attribute('href')
-        last_page = page.locator("text=/.*\\>\\>.*/").get_attribute('href')
-        articles = []
-        while current_page != None:
-            html = page.inner_html('#issues')
-            soup = BeautifulSoup(html, 'html.parser')
-            if last_page == page.url:
-                previous_page = page.locator("text=<").nth(1).get_attribute('href')
-                for all_links in soup.find_all('a'):
-                    if all_links.attrs['href'] != previous_page:
-                        articles.append(all_links.attrs['href'])
-                current_page = None
-            else:
-                next_page = page.locator("text=>").nth(1).get_attribute('href')
-                for all_links in soup.find_all('a'):
-                    if all_links.attrs['href'] != next_page:
-                        articles.append(all_links.attrs['href'])
-                page.locator("text=>").nth(1).click()
-            
-        return articles
+    articles = []
+    pass
+
+def getMagazines(html):
+    browser = p.firefox.launch(headless=False)
+    page = browser.new_page()
+    page.goto(html)
+    page.is_visible('div.tile-body')
+    condition = page.url
+    next_page = page.locator("text=>").nth(1).get_attribute('href')
+    last_page = page.locator("text=/.*\\>\\>.*/").get_attribute('href')
+    magazines = []
+    while condition != None:
+        html = page.inner_html('#issues')
+        soup = BeautifulSoup(html, 'html.parser')
+        if last_page == page.url:
+            previous_page = page.locator("text=<").nth(1).get_attribute('href')
+            for all_links in soup.find_all('a'):
+                if all_links.attrs['href'] != previous_page:
+                    magazines.append(all_links.attrs['href'])
+            condition = None
+        else:
+            next_page = page.locator("text=>").nth(1).get_attribute('href')
+            for all_links in soup.find_all('a'):
+                if all_links.attrs['href'] != next_page:
+                    magazines.append(all_links.attrs['href'])
+            page.locator("text=>").nth(1).click()
+        
+    return magazines
 
 with sync_playwright() as p:
-    print(getArticles('http://www.periodicos.ulbra.br/index.php/acta/issue/archive'))
+    print(getMagazines('http://www.periodicos.ulbra.br/index.php/acta/issue/archive'))
 
 
 
@@ -40,11 +52,11 @@ with sync_playwright() as p:
     page.goto('http://www.periodicos.ulbra.br/index.php/acta/issue/archive')
     html = page.inner_html('#issues')
     soup = BeautifulSoup(html, 'html.parser')
-    articles = []
+    Magazines = []
     next_page = page.locator("text=>").nth(1).get_attribute('href')
     for all_links in soup.find_all('a'):
         if all_links.attrs['href'] != next_page:
-            articles.append(all_links.attrs['href'])
+            Magazines.append(all_links.attrs['href'])
         
-    print(articles)
+    print(Magazines)
 '''
